f867725e3efd14f48dd0d8969bb1574cd51410d3
Sabir Hassan <sabirbk06@gmail.com>
2022-07-30 10:27:25 +0500

    chore: add layout component

---
+++ src/components/layout.tsx
@@ -0,0 +1,48 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import Footer from "./footer"
+import "../styles/index.scss"
+
+import { Row, Col } from "reactstrap"
+
+type Props = {
+  pageTitle: string
+  children?: React.ReactNode
+}
+
+const Layout = ({ pageTitle, children }: Props) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={(data) => (
+      <>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+          integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+          crossOrigin="anonymous"
+        />
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div className="container" id="content">
+          <h1>{pageTitle}</h1>
+          <Row>
+            <Col>{children}</Col>
+          </Row>
+        </div>
+        <Footer />
+      </>
+    )}
+  />
+)
+
+export default Layout
