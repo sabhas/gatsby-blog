528793f7c1991cd71be85df7a477269938d68a37
Sabir Hassan <sabirbk06@gmail.com>
2022-08-13 23:55:48 +0500

    chore: update pathPrefix config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,7 @@
 import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
-  pathPrefix: "/",
+  pathPrefix: "/gatsby-blog",
   siteMetadata: {
     title: `My Blog`,
     description: "My personal blog related to web technologies.",
